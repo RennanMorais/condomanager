4813567775467177ea9d43edd5aba65f2c404335
unknown <rennan.morais@outlook.com>
2020-06-24 15:29:04 -0300

    Atualizações da landing page 24-06-2020

---
+++ _MODELO/assets/js/script.js
@@ -0,0 +1,17 @@
+function scrollInicio(){
+    $('html, body').animate({
+      scrollTop: $(".section-banner").offset().top
+    }, 1000);
+}
+
+function scrollQuemSomos(){
+    $('html, body').animate({
+      scrollTop: $(".section-0").offset().top
+    }, 1000);
+}
+
+function scrollSistema(){
+    $('html, body').animate({
+      scrollTop: $(".section-1").offset().top
+    }, 1000);
+}
