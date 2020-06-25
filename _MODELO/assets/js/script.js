f3c35023d178e1f6b2cea23f14aeeec31bbfe03c
unknown <rennan.morais@outlook.com>
2020-06-25 14:23:38 -0300

    Desenvolvimento completo da parte visual da landing page 95% de conclusão 25-06-2020

--- _MODELO/assets/js/script.js
+++ _MODELO/assets/js/script.js
@@ -1,17 +1,23 @@
-function scrollInicio(){
+function scrollInicio() {
     $('html, body').animate({
       scrollTop: $(".section-banner").offset().top
     }, 1000);
 }
 
-function scrollQuemSomos(){
+function scrollQuemSomos() {
     $('html, body').animate({
       scrollTop: $(".section-0").offset().top
     }, 1000);
 }
 
-function scrollSistema(){
+function scrollSistema() {
     $('html, body').animate({
       scrollTop: $(".section-1").offset().top
+    }, 1000);
+}
+
+function scrollcontato() {
+    $('html, body').animate({
+      scrollTop: $(".footer").offset().top
     }, 1000);
 }
